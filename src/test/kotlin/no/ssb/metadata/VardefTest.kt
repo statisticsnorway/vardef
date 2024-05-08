b5377634ad975a2f853998dd74863c773459919a
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-05-08 08:54:03 +0200

    Upload failing test

--- src/test/kotlin/no/ssb/metadata/VardefTest.kt
+++ src/test/kotlin/no/ssb/metadata/VardefTest.kt
@@ -4,6 +4,7 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
 
 @MicronautTest(transactional = false)
 class VardefTest {
@@ -14,6 +15,12 @@ class VardefTest {
     @Test
     fun testItWorks() {
         Assertions.assertTrue(application.isRunning)
+    }
+
+    @Test
+    fun testWorkflowTestFails(){
+        val expected = 2
+        assertEquals(expected, 1)
     }
 
 }

e227fd4be4b275ec7699c487355ffe595b75c7c6
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-05-08 13:23:39 +0200

    Feat/workflow unit tests (#2)
    * Upload failing test
    
    * Test logging
    
    * Replace Hamcrest with Assertj
    
    * Push failing test
    
    * Add test logging
    
    * Add simple logger
    
    * Add Micronaut logback - update failing test
    
    * Change from get to indexing
    
    * Change Test without tasks test
    
    * Change test to assertJ - add logging to passed tests
    
    * add events
    
    * add passed test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.23"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.9.23"
@@ -12,7 +13,7 @@ plugins {
 version = "0.1"
 group = "no.ssb.metadata"
 
-val kotlinVersion=project.properties.get("kotlinVersion")
+val kotlinVersion= project.properties["kotlinVersion"]
 repositories {
     mavenCentral()
 }
@@ -29,6 +30,7 @@ dependencies {
     implementation("io.micronaut.serde:micronaut-serde-jackson")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("ch.qos.logback:logback-classic")
     compileOnly("io.micronaut:micronaut-http-client")
     compileOnly("io.micronaut.openapi:micronaut-openapi-annotations")
     runtimeOnly("ch.qos.logback:logback-classic")
@@ -36,11 +38,10 @@ dependencies {
     runtimeOnly("org.mongodb:mongodb-driver-reactivestreams")
     runtimeOnly("org.yaml:snakeyaml")
     testImplementation("io.micronaut:micronaut-http-client")
-    testImplementation("org.hamcrest:hamcrest")
+    testImplementation("org.assertj:assertj-core")
     aotPlugins(platform("io.micronaut.platform:micronaut-platform:4.4.2"))
     aotPlugins("io.micronaut.security:micronaut-security-aot")
 }
-
 
 application {
     mainClass = "no.ssb.metadata.ApplicationKt"
@@ -73,8 +74,13 @@ micronaut {
     }
 }
 
-
 tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
     jdkVersion = "21"
 }
 
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed", "standardOut", "standardError")
+    }
+}

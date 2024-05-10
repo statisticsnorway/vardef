3b76c3bfd2103cc3c445866cfc6e25359fb9cd25
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-05-10 09:23:37 +0200

    Remove reactive implementation

--- src/main/kotlin/no/ssb/metadata/controllers/VariablesController.kt
+++ src/main/kotlin/no/ssb/metadata/controllers/VariablesController.kt
@@ -2,32 +2,33 @@ package no.ssb.metadata.controllers
 
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.*
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
 import io.micronaut.validation.Validated
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.inject.Inject
 import jakarta.validation.Valid
 import no.ssb.metadata.models.VariableDefinition
 import no.ssb.metadata.repositories.VariableDefinitionRepository
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
 @Validated
 @Controller("/variables")
+@ExecuteOn(TaskExecutors.BLOCKING)
 class VariablesController {
 
     @Inject
     lateinit var vardefService: VariableDefinitionRepository
 
     @Get()
-    fun list(): Flux<VariableDefinition> {
-        return vardefService.list()
+    fun list(): List<VariableDefinition> {
+        return vardefService.findAll().toList()
     }
 
     @Post()
     @Status(HttpStatus.CREATED)
     @ApiResponse(responseCode = "201", description = "Successfully created.")
     @ApiResponse(responseCode = "400", description = "Bad request. See the message for more details.")
-    open fun createVariableDefinition(@Body @Valid vardef: VariableDefinition): Mono<VariableDefinition> {
+    open fun createVariableDefinition(@Body @Valid vardef: VariableDefinition): VariableDefinition {
         return vardefService.save(vardef)
     }
 }
--- src/main/kotlin/no/ssb/metadata/repositories/VariableDefinition.kt
+++ src/main/kotlin/no/ssb/metadata/repositories/VariableDefinition.kt
@@ -1,34 +1,10 @@
 package no.ssb.metadata.repositories
 
-import com.mongodb.reactivestreams.client.MongoClient
-import com.mongodb.reactivestreams.client.MongoCollection
-import io.micronaut.core.annotation.NonNull
-import jakarta.inject.Singleton
-import jakarta.validation.Valid
+import io.micronaut.data.mongodb.annotation.MongoRepository
+import io.micronaut.data.repository.CrudRepository
 import no.ssb.metadata.models.VariableDefinition
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
+import org.bson.types.ObjectId
 
-interface VariableDefinitionRepository {
 
-    fun list(): Flux<VariableDefinition>
-
-    fun save(@Valid variable: VariableDefinition): Mono<VariableDefinition>
-}
-
-@Singleton
-open class MongoDbVariableDefinitionRepository(
-    private val mongoClient: MongoClient
-) : VariableDefinitionRepository {
-
-    override fun save(@Valid variable: VariableDefinition): Mono<VariableDefinition> =
-        Mono.from(collection.insertOne(variable))
-            .map { variable }
-
-    @NonNull
-    override fun list(): Flux<VariableDefinition> = Flux.from(collection.find())
-
-    private val collection: MongoCollection<VariableDefinition>
-        get() = mongoClient.getDatabase("vardef")
-            .getCollection("variable_definitions", VariableDefinition::class.java)
-}
+@MongoRepository(databaseName = "vardef")
+interface VariableDefinitionRepository : CrudRepository<VariableDefinition, ObjectId>

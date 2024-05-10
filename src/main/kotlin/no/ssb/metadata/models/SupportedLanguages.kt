d344b782a338165bcee38e71c9b1d79dcbc1b0da
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-05-10 15:56:31 +0200

    Fix lower case serialization

--- src/main/kotlin/no/ssb/metadata/models/SupportedLanguages.kt
+++ src/main/kotlin/no/ssb/metadata/models/SupportedLanguages.kt
@@ -3,7 +3,12 @@ package no.ssb.metadata.models
 import io.micronaut.serde.annotation.Serdeable
 import io.micronaut.serde.config.naming.LowerCaseStrategy
 
-@Serdeable(naming=LowerCaseStrategy::class)
+@Serdeable(naming = LowerCaseStrategy::class)
 enum class SupportedLanguages {
-    NB, NN, EN
+    NB, // Norwegian Bokmål
+    NN, // Norwegian Nynorsk
+    EN, // English
+    ;
+
+    override fun toString() = name.lowercase()
 }
--- src/main/kotlin/no/ssb/metadata/models/VariableDefinition.kt
+++ src/main/kotlin/no/ssb/metadata/models/VariableDefinition.kt
@@ -5,12 +5,14 @@ import io.micronaut.data.annotation.GeneratedValue
 import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
 import io.micronaut.serde.annotation.Serdeable
-import io.micronaut.serde.config.naming.SnakeCaseStrategy
+import io.swagger.v3.oas.annotations.media.Schema
 import org.bson.types.ObjectId
 
-
 @MappedEntity
-@Serdeable(naming=SnakeCaseStrategy::class)
+@Serdeable
+@Schema(
+    example = "{\"name\": {\"en\": \"English\",\"nb\": \"Norwegian Bokmål\",\"nn\": \"Norwegian Nynorsk\"},\"short_name\": \"string\",\"definition\": {\"en\": \"English\",\"nb\": \"Norwegian Bokmål\",\"nn\": \"Norwegian Nynorsk\"}}",
+)
 data class VariableDefinition(
     @field:Id
     @GeneratedValue
@@ -18,7 +20,5 @@ data class VariableDefinition(
     var id: ObjectId?,
     var name: Map<SupportedLanguages, String>,
     var shortName: String?,
-    var definition: Map<SupportedLanguages, String>
+    var definition: Map<SupportedLanguages, String>,
 )
-
-

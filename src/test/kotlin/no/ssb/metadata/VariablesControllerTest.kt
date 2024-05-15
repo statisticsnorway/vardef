3a70b923e8750d41aca23f76aa1f778e01ec2217
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-05-15 14:34:18 +0200

    assert post respons in test

--- src/test/kotlin/no/ssb/metadata/VariablesControllerTest.kt
+++ src/test/kotlin/no/ssb/metadata/VariablesControllerTest.kt
@@ -3,6 +3,7 @@ package no.ssb.metadata
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import io.restassured.http.ContentType
 import io.restassured.specification.RequestSpecification
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.jupiter.api.Test
 
 @MicronautTest
@@ -13,11 +14,14 @@ class VariablesControllerTest {
         spec
             .given()
             .contentType(ContentType.JSON)
-            .body("{\"name\":{\"en\":\"value\",\"nb\":\"value\",\"nn\":\"value\" },\"shortName\":\"value\",\"definition\":{\"en\":\"value\",\"nb\":\"value\",\"nn\":\"value\" }}")
+            .body("{\"name\":{\"en\":\"Bank connections\",\"nb\":\"Bankforbindelser\",\"nn\":\"value\" },\"shortName\":\"Bank\",\"definition\":{\"en\":\"value\",\"nb\":\"value\",\"nn\":\"value\" }}")
             .`when`()
             .post("/variables")
             .then()
             .statusCode(201)
+            .body("shortName", equalTo("Bank"))
+            .body("name.nb", equalTo("Bankforbindelser"))
+
     }
 
     @Test
@@ -30,4 +34,3 @@ class VariablesControllerTest {
     }
 }
 
-// .body("{\"name\":\"name\",\"shortName\":\"value\",\"definition\":\"value\"}")

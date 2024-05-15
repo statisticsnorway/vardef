851f59cb3e2bcbd1bc75c069d46b5d094193a0a5
Joergen <rlj@ssb.no>
2024-05-15 15:03:39 +0200

    Updated api documentation

--- src/main/kotlin/no/ssb/metadata/Application.kt
+++ src/main/kotlin/no/ssb/metadata/Application.kt
@@ -2,13 +2,29 @@ package no.ssb.metadata
 
 import io.micronaut.runtime.Micronaut.run
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.extensions.Extension
+import io.swagger.v3.oas.annotations.extensions.ExtensionProperty
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.info.Contact
 
 @OpenAPIDefinition(
     info =
         Info(
-            title = "vardef",
-            version = "0.0",
+            title = "Variable Definitions",
+            description = "",
+            version = "0.1",
+            license = License(name = "MIT License", url = "https://opensource.org/licenses/MIT"),
+            contact = Contact(email = "mmw@ssb.no"),
+            extensions =
+            arrayOf(
+                Extension(
+                    properties =
+                    arrayOf(
+                        ExtensionProperty(name = "x-audience", value = "external-public"),
+                    ),
+                ),
+            ),
         ),
 )
 object Api
--- src/main/kotlin/no/ssb/metadata/models/VariableDefinition.kt
+++ src/main/kotlin/no/ssb/metadata/models/VariableDefinition.kt
@@ -5,10 +5,14 @@ import io.micronaut.data.annotation.GeneratedValue
 import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
 import io.micronaut.serde.annotation.Serdeable
+import io.swagger.v3.oas.annotations.media.Schema
 import org.bson.types.ObjectId
 
 @MappedEntity
 @Serdeable
+@Schema(
+    example = "{\"name\": {\"en\": \"English\",\"nb\": \"Norwegian Bokmål\",\"nn\": \"Norwegian Nynorsk\"},\"short_name\": \"string\",\"definition\": {\"en\": \"English\",\"nb\": \"Norwegian Bokmål\",\"nn\": \"Norwegian Nynorsk\"}}",
+)
 data class VariableDefinition(
     @field:Id
     @GeneratedValue

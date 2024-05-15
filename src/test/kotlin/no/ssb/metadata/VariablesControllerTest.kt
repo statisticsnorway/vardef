059c53d7e191dbebad0a1617d881cdc8205063dd
Joergen <rlj@ssb.no>
2024-05-15 14:08:11 +0200

    Updated model

--- src/test/kotlin/no/ssb/metadata/VariablesControllerTest.kt
+++ src/test/kotlin/no/ssb/metadata/VariablesControllerTest.kt
@@ -3,8 +3,6 @@ package no.ssb.metadata
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import io.restassured.http.ContentType
 import io.restassured.specification.RequestSpecification
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.hasItem
 import org.junit.jupiter.api.Test
 
 @MicronautTest

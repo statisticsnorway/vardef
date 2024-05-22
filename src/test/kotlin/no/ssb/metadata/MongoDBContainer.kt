e4f45dcd886aed60cc4eabfdf10b1819ceecb59d
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-05-22 14:06:13 +0200

    add testcontainers to gradle - add Container class

---
+++ src/test/kotlin/no/ssb/metadata/MongoDBContainer.kt
@@ -0,0 +1,46 @@
+package no.ssb.metadata
+
+import com.mongodb.assertions.Assertions.assertFalse
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+import org.testcontainers.utility.MountableFile
+
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.testcontainers.containers.MongoDBContainer
+
+@MicronautTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MongoDBContainer {
+
+    //@Inject private val mongoDBContainer: MongoDBContainer
+    companion object {
+        private lateinit var mongoDBContainer: MongoDBContainer
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            mongoDBContainer = MongoDBContainer("mongo:4.0.10")
+            mongoDBContainer.start()
+            System.setProperty("mongodb.uri", mongoDBContainer.replicaSetUrl)
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun teardown() {
+            mongoDBContainer.stop()
+        }
+    }
+
+    @Test
+    fun testSomething() {
+        // Your test logic here
+        assertFalse(false)
+    }
+}

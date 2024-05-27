edc943cb8ba7d05b1d073ced12a09453a1f79597
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-05-27 15:13:19 +0200

    Fix linting

--- src/main/kotlin/no/ssb/metadata/models/VariableDefinitionDAO.kt
+++ src/main/kotlin/no/ssb/metadata/models/VariableDefinitionDAO.kt
@@ -36,20 +36,22 @@ data class VariableDefinitionDAO(
     @field:NotEmpty val shortName: String,
     @field:NotEmpty val definition: Map<SupportedLanguages, String>,
 ) {
-    fun getName(language: String): String? = this.name
+    fun getName(language: String): String? =
+        this.name
             .map { it.key to it.value }
             .firstOrNull { it.first.toString() == language }
             ?.second
 
-    fun getDefinition(language: String): String? = this.definition
+    fun getDefinition(language: String): String? =
+        this.definition
             .map { it.key to it.value }
             .firstOrNull { it.first.toString() == language }
             ?.second
 
-    fun toDTO(language: String): VariableDefinitionDTO = VariableDefinitionDTO(
-        name = this.getName(language),
-        shortName = shortName,
-        definition = this.getDefinition(language)
-    )
-
+    fun toDTO(language: String): VariableDefinitionDTO =
+        VariableDefinitionDTO(
+            name = this.getName(language),
+            shortName = shortName,
+            definition = this.getDefinition(language),
+        )
 }

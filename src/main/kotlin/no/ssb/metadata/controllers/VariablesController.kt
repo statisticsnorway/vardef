3c055368b9fdbe93f08205998230ffa54ac1b4c7
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-05-28 12:57:41 +0200

    Include Content-Language header in responses (#25)
    * Response includes Content-Language header
    
    * Test Content-Language header

--- src/main/kotlin/no/ssb/metadata/controllers/VariablesController.kt
+++ src/main/kotlin/no/ssb/metadata/controllers/VariablesController.kt
@@ -1,18 +1,16 @@
 package no.ssb.metadata.controllers
 
+import io.micronaut.http.HttpHeaders
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Header
-import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.Status
+import io.micronaut.http.annotation.*
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
 import io.micronaut.validation.Validated
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.inject.Inject
 import jakarta.validation.Valid
+import no.ssb.metadata.models.SupportedLanguages
 import no.ssb.metadata.models.VariableDefinitionDAO
 import no.ssb.metadata.models.VariableDefinitionDTO
 import no.ssb.metadata.services.VariableDefinitionService
@@ -25,17 +23,19 @@ class VariablesController {
     lateinit var varDefService: VariableDefinitionService
 
     @Get()
-    fun listAllByLanguage(
-        @Header("Accept-Language", defaultValue = "nb") language: String,
-    ): List<VariableDefinitionDTO> {
-        return varDefService.findByLanguage(language)
+    fun listVariableDefinitions(
+        @Header("Accept-Language", defaultValue = "nb") language: SupportedLanguages,
+    ): HttpResponse<List<VariableDefinitionDTO>> {
+        return HttpResponse
+            .ok(varDefService.findByLanguage(language))
+            .header(HttpHeaders.CONTENT_LANGUAGE, language.toString())
     }
 
     @Post()
     @Status(HttpStatus.CREATED)
     @ApiResponse(responseCode = "201", description = "Successfully created.")
     @ApiResponse(responseCode = "400", description = "Bad request.")
-    fun save(
+    fun createVariableDefinition(
         @Body @Valid varDef: VariableDefinitionDAO,
     ): VariableDefinitionDAO = varDefService.save(varDef)
 }

56b75df4463a3de12807a026e9d40ba7d91dfcc7
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-06-07 12:46:19 +0200

    set up scheduled job - remove temporary controller

--- src/main/kotlin/no/ssb/metadata/vardef/integrations/klass/KlassJob.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/integrations/klass/KlassJob.kt
@@ -1,16 +1,31 @@
 package no.ssb.metadata.vardef.integrations.klass
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.scheduling.annotation.Scheduled
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
+import no.ssb.metadata.vardef.integrations.klass.models.KlassApiResponse
+import no.ssb.metadata.vardef.integrations.klass.service.KlassApiClient
 import org.slf4j.LoggerFactory
 import java.text.SimpleDateFormat
 import java.util.*
 
 @Singleton
 class KlassJob {
-    @Scheduled(cron = "0 00 13 * * ?")
-    fun getClassifications() {
-        LOG.info("Simulate getting classifications: {}", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()))
+
+    @Inject
+    lateinit var klassApiClient: KlassApiClient
+
+    @Scheduled(cron = "0 44 12 * * ?")
+    fun getClassifications(): HttpResponse<KlassApiResponse> {
+            return try {
+                val result = klassApiClient.fetchClassificationList()
+                LOG.info("Retrieving classifications from Klass Api {} {}", result, SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()))
+                HttpResponse.ok(result)
+            } catch (e: Exception) {
+                HttpResponse.notFound()
+            }
+
     }
 
     companion object {

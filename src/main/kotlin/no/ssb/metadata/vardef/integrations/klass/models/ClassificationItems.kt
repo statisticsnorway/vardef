d179879fedfadf1922d3b4f12bb532002d78de2c
tilen1976 <68303562+tilen1976@users.noreply.github.com>
2024-06-07 10:53:06 +0200

    remove unused classes

--- src/main/kotlin/no/ssb/metadata/vardef/integrations/klass/models/ClassificationItems.kt
+++
@@ -1,6 +0,0 @@
-package no.ssb.metadata.vardef.integrations.klass.models
-
-import io.micronaut.serde.annotation.Serdeable
-
-@Serdeable
-data class ClassificationItems(val codes: List<ClassificationItem>)

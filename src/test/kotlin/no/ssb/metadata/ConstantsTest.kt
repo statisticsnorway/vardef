3f1b92cccdebf84b5b5ee9c19be0d4336abef26b
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-06-17 08:41:33 +0200

    Validate schema example

--- src/test/kotlin/no/ssb/metadata/ConstantsTest.kt
+++ src/test/kotlin/no/ssb/metadata/ConstantsTest.kt
@@ -1,10 +1,13 @@
 package no.ssb.metadata
 
 import io.micronaut.json.JsonMapper
+import jakarta.inject.Inject
 import no.ssb.metadata.constants.INPUT_VARIABLE_DEFINITION_EXAMPLE
 import no.ssb.metadata.constants.RENDERED_VARIABLE_DEFINITION
+import no.ssb.metadata.constants.UPDATE_VARIABLE_DEFINITION
 import no.ssb.metadata.models.InputVariableDefinition
 import no.ssb.metadata.models.RenderedVariableDefinition
+import no.ssb.metadata.models.UpdateVariableDefinition
 import org.json.JSONObject
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Disabled
@@ -13,8 +16,11 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class ConstantsTest {
+    @Inject
+    lateinit var jsonMapper: JsonMapper
+
     @ParameterizedTest
-    @ValueSource(strings = [RENDERED_VARIABLE_DEFINITION, INPUT_VARIABLE_DEFINITION_EXAMPLE])
+    @ValueSource(strings = [RENDERED_VARIABLE_DEFINITION, INPUT_VARIABLE_DEFINITION_EXAMPLE, UPDATE_VARIABLE_DEFINITION])
     fun `examples are valid json`(string: String) {
         Assertions.assertNotNull(JSONObject(string))
     }
@@ -23,7 +29,7 @@ class ConstantsTest {
     @Test
     fun `rendered variable definition example is valid`() {
         Assertions.assertNotNull(
-            JsonMapper.createDefault().readValue(RENDERED_VARIABLE_DEFINITION, RenderedVariableDefinition::class.java),
+            jsonMapper.readValue(RENDERED_VARIABLE_DEFINITION, RenderedVariableDefinition::class.java),
         )
     }
 
@@ -31,7 +37,15 @@ class ConstantsTest {
     @Test
     fun `input variable definition example is valid`() {
         Assertions.assertNotNull(
-            JsonMapper.createDefault().readValue(INPUT_VARIABLE_DEFINITION_EXAMPLE, InputVariableDefinition::class.java),
+            jsonMapper.readValue(INPUT_VARIABLE_DEFINITION_EXAMPLE, InputVariableDefinition::class.java),
+        )
+    }
+
+    @Disabled("TODO: Complete under DPMETA-257")
+    @Test
+    fun `update variable definition example is valid`() {
+        Assertions.assertNotNull(
+            jsonMapper.readValue(UPDATE_VARIABLE_DEFINITION, UpdateVariableDefinition::class.java),
         )
     }
 }

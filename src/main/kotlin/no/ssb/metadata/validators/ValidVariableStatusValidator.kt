83920f3e9615e3b2fd83f7b29cb596144e9cb234
Joergen <rlj@ssb.no>
2024-06-18 15:48:13 +0200

    Linting

--- src/main/kotlin/no/ssb/metadata/validators/ValidVariableStatusValidator.kt
+++ src/main/kotlin/no/ssb/metadata/validators/ValidVariableStatusValidator.kt
@@ -2,7 +2,6 @@ package no.ssb.metadata.validators
 
 import no.ssb.metadata.models.VariableStatus
 
-
 object ValidVariableStatusValidator {
     fun isValid(value: String): Boolean {
         return VariableStatus.entries.any { it.name == value }

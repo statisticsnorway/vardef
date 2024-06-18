b3d2b9c65ffb53fbb0dac14cf4fb5aa8862ca3d0
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-06-18 12:27:45 +0200

    Regex patterns as constants (#49)

---
+++ src/main/kotlin/no/ssb/metadata/constants/Patterns.kt
@@ -0,0 +1,7 @@
+package no.ssb.metadata.constants
+
+const val DATE_PATTERN = "^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$"
+const val URL_PATTERN = "^https://[^\\s/$.?#].\\S*$"
+const val KLASS_ID_PATTERN = "^[0-9]+$"
+const val VARDEF_SHORT_NAME_PATTERN = "^[a-z0-9_]{3,}$"
+const val VARDEF_ID_PATTERN = "^[a-zA-Z0-9-_]{8}$"

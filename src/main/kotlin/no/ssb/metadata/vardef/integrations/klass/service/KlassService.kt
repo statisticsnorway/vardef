6c8d3a035776638db4fa9b98a758fc1413243874
Joergen <rlj@ssb.no>
2024-07-01 12:31:50 +0200

    Removed unused import

--- src/main/kotlin/no/ssb/metadata/vardef/integrations/klass/service/KlassService.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/integrations/klass/service/KlassService.kt
@@ -4,7 +4,6 @@ import io.micronaut.context.annotation.Prototype
 import io.micronaut.core.annotation.Introspected
 import no.ssb.metadata.models.KlassReference
 import no.ssb.metadata.models.SupportedLanguages
-import no.ssb.metadata.vardef.integrations.klass.models.ClassificationItem
 
 @Prototype
 @Introspected

ce1d516f6da395455b301fced469f192c665d6a6
Jorgen-5 <rlj@ssb.no>
2024-08-26 14:27:56 +0200

    Fixed tests

--- src/test/kotlin/no/ssb/metadata/vardef/utils/BaseVardefTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/utils/BaseVardefTest.kt
@@ -31,6 +31,7 @@ open class BaseVardefTest {
         variableDefinitionService.save(SAVED_VARIABLE_DEFINITION_COPY)
         variableDefinitionService.save(SAVED_VARIABLE_DEFINITION.apply { patchId = 2 })
         variableDefinitionService.save(SAVED_VARIABLE_DEFINITION.apply { patchId = 3 })
+        variableDefinitionService.save(SAVED_DRAFT_VARIABLE_DEFINITION)
         variableDefinitionService.save(
             SAVED_VARIABLE_DEFINITION.apply {
                 validFrom = LocalDate.of(1980, 12, 1)
@@ -52,6 +53,5 @@ open class BaseVardefTest {
         variableDefinitionService.save(INPUT_VARIABLE_DEFINITION.toSavedVariableDefinition())
         variableDefinitionService.save(INPUT_VARIABLE_DEFINITION_COPY.toSavedVariableDefinition())
         variableDefinitionService.save(INPUT_VARIABLE_DEFINITION_NO_NAME.toSavedVariableDefinition())
-        variableDefinitionService.save(SAVED_DRAFT_VARIABLE_DEFINITION)
     }
 }

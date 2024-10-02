e1710ad16fc012bb07fbee6613e04abec13a59fd
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-10-02 09:55:56 +0200

    Remove unused function

--- src/main/kotlin/no/ssb/metadata/vardef/integrations/vardok/Convertions.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/integrations/vardok/Convertions.kt
@@ -1,7 +1,5 @@
 package no.ssb.metadata.vardef.integrations.vardok
 
-import no.ssb.metadata.vardef.models.Owner
-
 val unitTypeConverter =
     mapOf(
         "Adresse" to "01",
@@ -45,10 +43,4 @@ fun mapVardokIdentifier(vardokItem: VardokResponse): String {
     val vardokId = vardokItem.id
     val splitId = vardokId.split(":")
     return splitId[splitId.size - 1]
-}
-
-fun mapVardokContactDivisionToOwner(vardokItem: VardokResponse): Owner {
-    val owner = vardokItem.common?.contactDivision
-    val mappedOwner = Owner(owner!!.codeValue)
-    return mappedOwner
 }

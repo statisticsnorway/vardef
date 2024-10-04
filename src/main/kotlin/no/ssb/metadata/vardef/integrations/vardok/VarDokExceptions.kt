632a0cfd90bfea92a1a83cced24dddfad5bdeab3
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-10-04 13:34:38 +0200

    remove default value exception messages

--- src/main/kotlin/no/ssb/metadata/vardef/integrations/vardok/VarDokExceptions.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/integrations/vardok/VarDokExceptions.kt
@@ -3,26 +3,26 @@ package no.ssb.metadata.vardef.integrations.vardok
 open class VardokException(override val message: String, val id: String?) : Exception()
 
 class MissingDataElementNameException(id: String) :
-    VardokException("Vardok id $id is missing DataElementName (short name) and can not be saved", id = null)
+    VardokException("Vardok id $id is missing DataElementName (short name) and can not be saved", id)
 
 class MissingValidDatesException(id: String) :
-    VardokException("Vardok id $id is missing Valid (valid dates) and can not be saved", id = null)
+    VardokException("Vardok id $id is missing Valid (valid dates) and can not be saved", id)
 
 class MissingValidFromException(id: String) : VardokException(
     "Vardok id $id Valid is missing 'from' date and can not be saved",
     id = null,
 )
 
-class VardokNotFoundException(id: String) : VardokException("Vardok id $id not found", id = null)
+class VardokNotFoundException(id: String) : VardokException("Vardok id $id not found", id)
 
 class IllegalShortNameException(id: String) : VardokException(
     "Vardok id $id DataElementName does not conform to short name rules and can not be saved",
-    id = null,
+    id,
 )
 
 class OutdatedUnitTypesException(id: String) : VardokException(
     "Vardok id $id StatisticalUnit has outdated unit types and can not be saved",
-    id = null,
+    id,
 )
 
 fun checkVardokForMissingElements(varDokItems: MutableMap<String, VardokResponse>) {

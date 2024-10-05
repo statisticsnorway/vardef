c49731a2c8c37c3104fb9c1be9583b766737fefe
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-10-05 16:45:35 +0200

    add rule for running external service in tests

--- src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/VarDokMigrationTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/VarDokMigrationTest.kt
@@ -2,6 +2,7 @@ package no.ssb.metadata.vardef.integrations.vardok
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.micronaut.context.annotation.Requires
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import no.ssb.metadata.vardef.integrations.vardok.utils.vardokId1466validFromDateAndOtherLanguages
@@ -12,7 +13,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-// @Requires(env = ["integration-test"])
+@Requires(env = ["integration-test"])
 @MicronautTest
 class VarDokMigrationTest {
     @Inject

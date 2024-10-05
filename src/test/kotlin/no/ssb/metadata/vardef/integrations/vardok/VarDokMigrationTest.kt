054d1c5218d2011c9140ac96dd0d760df638ffc0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-10-05 17:06:51 +0200

    test code coverage

--- src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/VarDokMigrationTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/VarDokMigrationTest.kt
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-@Requires(env = ["integration-test"])
+//@Requires(env = ["integration-test"])
 @MicronautTest
 class VarDokMigrationTest {
     @Inject

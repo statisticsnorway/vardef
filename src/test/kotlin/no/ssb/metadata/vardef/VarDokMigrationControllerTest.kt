e9d3a35895a5619f8feb0782b7459a48918d8101
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-10-10 08:49:58 +0200

    Fix validation vardok migration (#148)
    * check unique short name - one failing test
    
    * check unique short name - one failing test
    
    * Check why test failed
    
    * rename
    
    * slim down integration tests
    
    * add rule for running external service in tests
    
    * refactor
    
    * test code coverage
    
    * test code coverage
    
    * re-add required rule
    
    * remove required
    
    * remove double check on short name
    
    * remove double check on short name
    
    * remove double check on short name pattern
    
    * update after cherry-pick

--- src/test/kotlin/no/ssb/metadata/vardef/VarDokMigrationControllerTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/VarDokMigrationControllerTest.kt
@@ -12,7 +12,7 @@ class VarDokMigrationControllerTest : BaseVardefTest() {
     @ParameterizedTest
     @ValueSource(
         ints = [
-            2, 164, 171, 173, 177, 683, 687, 703, 730, 746, 752, 754, 755, 756, 753, 871, 935, 936, 937, 948,
+            2, 164, 171, 687, 703, 871, 948,
         ],
     )
     fun `post request default language`(
@@ -27,6 +27,33 @@ class VarDokMigrationControllerTest : BaseVardefTest() {
             .post("/vardok-migration/$id")
             .then()
             .statusCode(201)
+    }
+
+    @Test
+    fun `post duplicate short name`(spec: RequestSpecification) {
+        spec
+            .given()
+            .contentType(ContentType.JSON)
+            .body("")
+            .`when`()
+            .post("/vardok-migration/2")
+            .then()
+            .statusCode(201)
+
+        spec
+            .given()
+            .contentType(ContentType.JSON)
+            .body("")
+            .`when`()
+            .post("/vardok-migration/2")
+            .then()
+            .statusCode(400)
+            .body(
+                "_embedded.errors[0].message",
+                containsString(
+                    "Short name wies already exists.",
+                ),
+            )
     }
 
     @ParameterizedTest
@@ -147,7 +174,7 @@ class VarDokMigrationControllerTest : BaseVardefTest() {
     @ParameterizedTest
     @ValueSource(
         ints = [
-            69, 141, 590, 1997, 2103, 2124, 2139, 2141, 2142, 2149, 2157, 2159, 2163, 2183, 2194, 2206, 2216, 2217,
+            69, 141, 590, 2157, 2159, 2163, 2217,
         ],
     )
     fun `post vardok missing updated statistical unit`(
@@ -173,7 +200,7 @@ class VarDokMigrationControllerTest : BaseVardefTest() {
     @ParameterizedTest
     @ValueSource(
         ints = [
-            16, 20, 161, 190, 476, 716, 1161, 1162, 1163, 1660, 2012, 2873, 3057, 3087, 3252, 3364, 3365, 1396,
+            16, 20, 161, 190, 476, 716, 1396, 1660, 2012, 3364, 3365,
         ],
     )
     fun `vardok dataelement name does not conform to short name rules`(
@@ -191,7 +218,7 @@ class VarDokMigrationControllerTest : BaseVardefTest() {
             .body(
                 "_embedded.errors[0].message",
                 containsString(
-                    "Vardok id $id DataElementName does not conform to short name rules and can not be saved",
+                    "varDef.shortName: must match \"^[a-z0-9_]{3,}$\"",
                 ),
             )
     }

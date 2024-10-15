4dfdfe3509b042e1488c10a953263b1ea8bfd9f6
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-10-15 12:51:28 +0200

    code 20 always renders to person

--- src/test/kotlin/no/ssb/metadata/vardef/models/RenderedVariableDefinitionTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/models/RenderedVariableDefinitionTest.kt
@@ -6,6 +6,7 @@ import no.ssb.metadata.vardef.utils.BaseVardefTest
 import no.ssb.metadata.vardef.utils.INCOME_TAX_VP1_P1
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 class RenderedVariableDefinitionTest : BaseVardefTest() {
     @Inject
@@ -18,5 +19,25 @@ class RenderedVariableDefinitionTest : BaseVardefTest() {
                 unitTypes = listOf("20"),
             ).render(SupportedLanguages.NB, klassService)
         assertThat(savedVariableDefinitionRendered.unitTypes[0]?.title).isEqualToIgnoringCase("person")
+    }
+
+    @Test
+    fun `unit type code 20 title is person regardless of date`()  {
+        val dates = listOf<LocalDate>(
+            LocalDate.of(1970, 1, 1),
+            LocalDate.of(1980, 12, 31),
+            LocalDate.of(2015, 1, 31),
+            LocalDate.of(2019, 5, 8),
+            LocalDate.of(2024, 11, 25),
+            LocalDate.of(2060, 3, 2),)
+        for (date in dates) {
+            val savedVariableDefinitionRendered =
+                INCOME_TAX_VP1_P1.copy(
+                    unitTypes = listOf("20"),
+                    validFrom = date
+                ).render(SupportedLanguages.NB, klassService)
+            assertThat(savedVariableDefinitionRendered.unitTypes[0]?.title).isEqualToIgnoringCase("person")
+        }
+
     }
 }

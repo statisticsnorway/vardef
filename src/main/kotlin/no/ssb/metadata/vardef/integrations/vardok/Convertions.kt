5e908c86229ac0c5f0a63b1247fcfadafc122e51
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-10-16 06:38:10 +0200

    updated mapping of unit types

--- src/main/kotlin/no/ssb/metadata/vardef/integrations/vardok/Convertions.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/integrations/vardok/Convertions.kt
@@ -1,48 +1,6 @@
 package no.ssb.metadata.vardef.integrations.vardok
 
-val unitTypeConverter =
-    mapOf(
-        "Adresse" to "01",
-        "Arbeidsulykke" to "02",
-        "Bolig" to "03",
-        "Bygning" to "04",
-        "Tinglyst omsetning" to "05",
-        "Skogareal" to "05",
-        "Landbrukseiendom" to "05",
-        "Grunneiendom" to "05",
-        "Eiendom" to "05",
-        "Familie" to "06",
-        "Fylke (forvaltning)" to "07",
-        "Fylke (geografisk)" to "08",
-        "Havneanløp" to "09",
-        "Husholdning" to "10",
-        "Juridisk enhet" to "11",
-        "Foretak" to "12",
-        "Virksomhet" to "13",
-        "Bedrift" to "13",
-        "Bransjeenhet" to "14",
-        "Kjøretøy" to "15",
-        "Kommune (forvaltning)" to "16",
-        "Kommune (geografisk)" to "17",
-        "Kurs" to "18",
-        "Lovbrudd" to "19",
-        "Arbeidsforhold" to "20",
-        "Person" to "20",
-        "Fiskefartøy" to "21",
-        "Skip" to "21",
-        "Offentlig forvaltning" to "22",
-        "Statlig Virksomhet" to "22",
-        "Storfe" to "23",
-        "Ulykke" to "24",
-        "Trafikkulykke" to "24",
-        "Transaksjon" to "25",
-        "Valg" to "26",
-        "Repr.vare og -tjeneste" to "27",
-        "Internett-abonnement" to "27",
-        "Vare/tjeneste" to "27",
-        "Verdipapir" to "28",
-    )
-
+// List of names that can be mapped to unitType codes
 val statisticalUnitNames = listOf(
     "Adresse",
     "Arbeidsulykke",
@@ -88,7 +46,14 @@ val statisticalUnitNames = listOf(
     "Luftfartøy",
     "sak"
 )
-// mapping ref:
+
+/**
+ * This converter map statisticalUnit (title) with unitType codes.
+ * Some statisticalUnits are outdated and are mapped according to set rules.
+ *
+ * @param name: statisticalUnit in Vardok
+ * @return A list of codes
+ */
 fun convertUnitTypes(name: String): List<String?>{
     val unitTypes = mutableListOf<String?>()
     when(name){
@@ -149,11 +114,6 @@ fun mapVardokStatisticalUnitToUnitTypes(vardokItem: VardokResponse): List<String
     if (statisticalUnit != null && statisticalUnitNames.contains(statisticalUnit)) {
         return convertUnitTypes(statisticalUnit)
     }
-    /*
-     if (statisticalUnit != null && unitTypeConverter.contains(statisticalUnit)) {
-        return listOf(unitTypeConverter[statisticalUnit])
-    }
-     */
 
     throw OutdatedUnitTypesException(vardokItem.id.substringAfterLast(":"))
 }

71c981a32a2601c4fc529d5b55eb4386811ce133
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-10-16 15:18:19 +0200

    Add KDoc to VariableDefinitionService

--- src/main/kotlin/no/ssb/metadata/vardef/services/VariableDefinitionService.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/services/VariableDefinitionService.kt
@@ -8,16 +8,41 @@ import no.ssb.metadata.vardef.models.SupportedLanguages
 import no.ssb.metadata.vardef.repositories.VariableDefinitionRepository
 import java.time.LocalDate
 
+/**
+ * *Variable Definition* service.
+ *
+ * Functionality pertaining to entire *Variable Definitions*, where versioning is not directly visible.
+ *
+ * @property variableDefinitionRepository The [VariableDefinitionRepository] Bean to be injected
+ * @property klassService The [KlassService] Bean to be injected
+ * @property validityPeriods The [ValidityPeriodsService] Bean to be injected
+ * @constructor Create empty Variable definition service
+ */
 @Singleton
 class VariableDefinitionService(
     private val variableDefinitionRepository: VariableDefinitionRepository,
     private val klassService: KlassService,
     private val validityPeriods: ValidityPeriodsService,
 ) {
+    /**
+     * Update a Draft Variable Definition
+     *
+     * @param varDef The object with updates applied
+     * @return The same object
+     */
     fun update(varDef: SavedVariableDefinition): SavedVariableDefinition = variableDefinitionRepository.update(varDef)
 
+    /**
+     * List all objects in the repository
+     */
     fun list(): List<SavedVariableDefinition> = variableDefinitionRepository.findAll()
 
+    /**
+     * Does the given short name already exist?
+     *
+     * @param shortName The value to check
+     * @return `true` if the [shortName] exists, otherwise `false`
+     */
     fun doesShortNameExist(shortName: String): Boolean = variableDefinitionRepository.existsByShortName(shortName)
 
     private fun uniqueDefinitionIds(): Set<String> =
@@ -25,6 +50,15 @@ class VariableDefinitionService(
             .map { it.definitionId }
             .toSet()
 
+    /**
+     * List *Variable Definitions* which are valid on the given date.
+     *
+     * If no date is given, list all variable definitions.
+     *
+     * @param language The language to render in.
+     * @param dateOfValidity The date which *Variable Definitions* shall be valid at.
+     * @return List of *Variable Definitions* valid at the date.
+     */
     fun listForDateAndRender(
         language: SupportedLanguages,
         dateOfValidity: LocalDate?,
@@ -34,6 +68,17 @@ class VariableDefinitionService(
                 getByDateAndRender(language, it, dateOfValidity)
             }
 
+    /**
+     * One rendered *Variable Definition*, valid at the given date.
+     *
+     * - If [dateOfValidity] is `null`, get the latest valid version.
+     * - If the variable is not valid on the given [dateOfValidity], return `null`
+     *
+     * @param language The language to render in.
+     * @param definitionId The ID of the *Variable Definition* of interest.
+     * @param dateOfValidity The date which the *Variable Definition* shall be valid at.
+     * @return The *Variable Definition* or `null`
+     */
     fun getByDateAndRender(
         language: SupportedLanguages,
         definitionId: String,

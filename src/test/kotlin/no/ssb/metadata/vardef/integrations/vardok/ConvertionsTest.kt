ee488dc1215a59c0db19c23ff70283bcca72726c
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-10-17 17:10:59 +0200

    refactor tests

--- src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/ConvertionsTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/ConvertionsTest.kt
@@ -9,7 +9,6 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 class ConvertionsTest : BaseVardokTest() {
-
     companion object {
         @JvmStatic
         fun unitTypes(): Stream<Arguments> =

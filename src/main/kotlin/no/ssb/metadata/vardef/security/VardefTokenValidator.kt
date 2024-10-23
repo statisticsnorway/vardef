4c6f465a23b571d1af84d9215e0373025c583817
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-10-23 10:26:34 +0200

    Factor out assignRoles function

--- src/main/kotlin/no/ssb/metadata/vardef/security/VardefTokenValidator.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/security/VardefTokenValidator.kt
@@ -24,6 +24,16 @@ class VardefTokenValidator<R> : ReactiveJsonWebTokenValidator<JWT, R> {
     @Inject
     private lateinit var jsonWebTokenParser: JsonWebTokenParser<JWT>
 
+    private fun assignRoles(
+        token: JWT,
+        request: R,
+    ): Roles =
+        if (daplaLabAudience in token.jwtClaimsSet.getStringListClaim(Claims.AUDIENCE)) {
+            Roles.VARIABLE_OWNER
+        } else {
+            Roles.VARIABLE_CONSUMER
+        }
+
     override fun validateToken(
         token: String?,
         request: R,
@@ -31,12 +41,12 @@ class VardefTokenValidator<R> : ReactiveJsonWebTokenValidator<JWT, R> {
         Mono
             .from(validate(token!!, request))
             .map {
-                if (daplaLabAudience in it.jwtClaimsSet.getStringListClaim(Claims.AUDIENCE)) {
-                    return@map Roles.VARIABLE_OWNER.name
-                } else {
-                    Roles.VARIABLE_CONSUMER.name
-                }
-            }.map { Authentication.build("", listOf(it), mapOf()) }
+                Authentication.build(
+                    it.jwtClaimsSet.getStringClaim("preferred_username"),
+                    listOf(assignRoles(it, request).name),
+                    it.jwtClaimsSet.claims,
+                )
+            }
 
     override fun validate(
         token: String?,

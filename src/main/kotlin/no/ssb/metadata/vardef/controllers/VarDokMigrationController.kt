7da732bf4e2e1fe4cb581cba30072d87b06e626b
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-10-30 13:54:21 +0100

    Import annotation

--- src/main/kotlin/no/ssb/metadata/vardef/controllers/VarDokMigrationController.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/controllers/VarDokMigrationController.kt
@@ -18,6 +18,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.inject.Inject
 import no.ssb.metadata.vardef.constants.*

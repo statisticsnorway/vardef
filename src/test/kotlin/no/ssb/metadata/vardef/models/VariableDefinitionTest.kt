82accd8ff0deb53aa2b94cd39ed1d8798d2cf1aa
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-11-01 15:46:15 +0100

    add test add group - remove redundant exception handling

--- src/test/kotlin/no/ssb/metadata/vardef/models/VariableDefinitionTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/models/VariableDefinitionTest.kt
@@ -3,7 +3,6 @@ package no.ssb.metadata.vardef.models
 import no.ssb.metadata.vardef.utils.*
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance

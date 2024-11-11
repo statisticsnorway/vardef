4e4e9d7c859a51e9f041c14dcd50127ec29033c9
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-11-11 10:48:44 +0100

    refactor tests

--- src/test/kotlin/no/ssb/metadata/vardef/integrations/dapla/ValidateDaplaTeamTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/integrations/dapla/ValidateDaplaTeamTest.kt
@@ -2,7 +2,6 @@ package no.ssb.metadata.vardef.integrations.dapla
 
 import io.micronaut.context.BeanContext
 import io.micronaut.inject.qualifiers.Qualifiers
-import io.micronaut.json.JsonMapper
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import io.micronaut.validation.validator.Validator
 import jakarta.inject.Inject
@@ -12,7 +11,6 @@ import no.ssb.metadata.vardef.integrations.dapla.service.StaticDaplaTeamService
 import no.ssb.metadata.vardef.models.Owner
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -24,9 +22,6 @@ class ValidateDaplaTeamTest(private val validator: Validator) {
 
     @Inject
     lateinit var beanContext: BeanContext
-
-    @Inject
-    lateinit var jsonMapper: JsonMapper
 
     @Test
     fun`validate team name`() {
@@ -101,26 +96,13 @@ class ValidateDaplaTeamTest(private val validator: Validator) {
 
     @Test
     fun `test StaticDaplaTeam loads correct data`() {
-        val teamA = beanContext.getBean(StaticDaplaTeam::class.java, Qualifiers.byName("dapla-felles"))
-        assertEquals("dapla-felles", teamA.uniformName)
+        val team = beanContext.getBean(StaticDaplaTeam::class.java, Qualifiers.byName("dapla-felles"))
+        assertEquals("dapla-felles", team.uniformName)
     }
 
     @Test
     fun `test StaticDaplaGroup loads correct data`() {
         val group = beanContext.getBean(StaticDaplaGroup::class.java, Qualifiers.byName("dapla-felles-developers"))
         assertEquals("dapla-felles-developers", group.uniformName)
-    }
-
-    @Test
-    fun `test StaticDaplaTeam is valid`() {
-        val result = daplaTeamApiStaticService.isValidTeam("dapla-felles")
-        assertThat(result).isTrue()
-        val result2 = daplaTeamApiStaticService.isValidTeam("min-min")
-        assertThat(result2).isFalse()
-    }
-
-    @Test
-    fun `test JsonMapper injection`() {
-        assertNotNull(jsonMapper) // Ensure the JsonMapper bean is injected
     }
 }

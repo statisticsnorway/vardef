5565491beb69b0de0f294e53f0d06d948b42cbe0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-11-14 19:46:50 +0100

    add KDoc

--- src/main/kotlin/no/ssb/metadata/vardef/integrations/dapla/services/DaplaTeamService.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/integrations/dapla/services/DaplaTeamService.kt
@@ -5,6 +5,14 @@ import io.micronaut.core.annotation.Introspected
 import no.ssb.metadata.vardef.integrations.dapla.models.Group
 import no.ssb.metadata.vardef.integrations.dapla.models.Team
 
+/**
+ * Interface representing a service for retrieving and validating teams and groups.
+ * This service provides methods to fetch `Team` and `Group` objects by name, and to check
+ * whether a given team or group exists.
+ *
+ * @Prototype Indicates that an instance of the implementing class should be created for each use.
+ * @Introspected Marks the interface for introspection, typically used for reflection or metadata processing.
+ */
 @Prototype
 @Introspected
 interface DaplaTeamService {

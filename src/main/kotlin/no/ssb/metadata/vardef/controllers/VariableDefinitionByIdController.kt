2026a34c16a7696c16d7aa1575385d570dccd6a3
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-12-17 12:35:21 +0100

    remove deprecated in messages

--- src/main/kotlin/no/ssb/metadata/vardef/controllers/VariableDefinitionByIdController.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/controllers/VariableDefinitionByIdController.kt
@@ -130,7 +130,7 @@ class VariableDefinitionByIdController(
         if (patches.latest(definitionId).variableStatus != VariableStatus.DRAFT) {
             throw HttpStatusException(
                 HttpStatus.METHOD_NOT_ALLOWED,
-                "The variable is published or deprecated and cannot be updated with this method",
+                "The variable is published and cannot be updated with this method",
             )
         }
 
@@ -199,7 +199,7 @@ class VariableDefinitionByIdController(
         when {
             variable.variableStatus != VariableStatus.DRAFT -> throw HttpStatusException(
                 HttpStatus.METHOD_NOT_ALLOWED,
-                "The variable is published or deprecated and cannot be updated with this method",
+                "The variable is published and cannot be updated with this method",
             )
 
             (

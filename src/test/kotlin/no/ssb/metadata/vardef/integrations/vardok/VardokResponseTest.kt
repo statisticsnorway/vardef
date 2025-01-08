6111867b228066e27596fc8e59c4541e1a9a7850
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-01-08 14:57:29 +0100

    add test vardok item - make test static

--- src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/VardokResponseTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/VardokResponseTest.kt
@@ -4,24 +4,25 @@ import io.micronaut.context.annotation.Requires
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import no.ssb.metadata.vardef.integrations.vardok.client.VardokClient
+import no.ssb.metadata.vardef.integrations.vardok.services.VardokService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-@Requires(env = ["integration-test"])
 @MicronautTest
 class VardokResponseTest {
+
     @Inject
-    lateinit var vardokClient: VardokClient
+    lateinit var vardokService: VardokService
 
     @Test
     fun `calculation in response`()  {
-        val response = vardokClient.fetchVardokById("566")
-        assertThat(response.variable?.calculation).isNotNull
+        val response = vardokService.getVardokItem("566")
+        assertThat(response?.variable?.calculation).isNotNull
     }
 
     @Test
     fun `calculation not in response`()  {
-        val response = vardokClient.fetchVardokById("2")
-        assertThat(response.variable?.calculation).isEmpty()
+        val response = vardokService.getVardokItem("2")
+        assertThat(response?.variable?.calculation).isEmpty()
     }
 }

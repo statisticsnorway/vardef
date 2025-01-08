5ee7a030b39d957cadfb8bfc8f4d2cc2b6432cf0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-01-08 10:26:26 +0100

    Update src/test/kotlin/no/ssb/metadata/vardef/controllers/patches/CreateTests.kt
    Co-authored-by: Jorgen-5 <jrussnes@gmail.com>

--- src/test/kotlin/no/ssb/metadata/vardef/controllers/patches/CreateTests.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/controllers/patches/CreateTests.kt
@@ -232,7 +232,7 @@ class CreateTests : BaseVardefTest() {
                         }.toPatch(),
                     DRAFT_BUS_EXAMPLE.definitionId,
                     DRAFT_BUS_EXAMPLE,
-                    "ano@ssb.no",
+                    TEST_USER,
                 ).definitionId
         spec
             .given()

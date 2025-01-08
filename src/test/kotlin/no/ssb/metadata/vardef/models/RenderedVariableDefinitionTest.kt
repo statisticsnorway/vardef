cbaa22ba1243c6b25155c450f8ad5e034df3d1da
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-01-08 10:26:15 +0100

    Update src/test/kotlin/no/ssb/metadata/vardef/models/RenderedVariableDefinitionTest.kt
    Co-authored-by: Jorgen-5 <jrussnes@gmail.com>

--- src/test/kotlin/no/ssb/metadata/vardef/models/RenderedVariableDefinitionTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/models/RenderedVariableDefinitionTest.kt
@@ -192,7 +192,7 @@ class RenderedVariableDefinitionTest : BaseVardefTest() {
 
     @Test
     fun `copyAndUpdate updates classificationReference to expected value`() {
-        val updatedVariable = DRAFT_BUS_EXAMPLE.copyAndUpdate(UPDATE_DRAFT_CLASSIFICATION_REFERENCE, "ano@ssb.no")
+        val updatedVariable = DRAFT_BUS_EXAMPLE.copyAndUpdate(UPDATE_DRAFT_CLASSIFICATION_REFERENCE, TEST_USER)
         assertThat(updatedVariable.classificationReference).isEqualTo("92")
     }
 

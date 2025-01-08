e8899caaaf01e0656737da44965ac72e9ca6b3f7
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-01-08 16:32:47 +0100

    Make Vardok Calculation field available (#282)
    * add calculation
    
    * ktlint
    
    * add test vardok item - make test static
    
    * ktlint

---
+++ src/test/kotlin/no/ssb/metadata/vardef/integrations/vardok/VardokResponseTest.kt
@@ -0,0 +1,25 @@
+package no.ssb.metadata.vardef.integrations.vardok
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import no.ssb.metadata.vardef.integrations.vardok.services.VardokService
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class VardokResponseTest {
+    @Inject
+    lateinit var vardokService: VardokService
+
+    @Test
+    fun `calculation in response`() {
+        val response = vardokService.getVardokItem("566")
+        assertThat(response?.variable?.calculation).isNotNull
+    }
+
+    @Test
+    fun `calculation not in response`() {
+        val response = vardokService.getVardokItem("2")
+        assertThat(response?.variable?.calculation).isEmpty()
+    }
+}

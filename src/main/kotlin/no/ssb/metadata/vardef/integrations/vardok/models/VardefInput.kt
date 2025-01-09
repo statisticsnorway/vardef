41dab04ae0a2a00e0346889cadcefac1fcd78366
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-01-09 09:48:17 +0100

    add 'comment' to VardokInput

--- src/main/kotlin/no/ssb/metadata/vardef/integrations/vardok/models/VardefInput.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/integrations/vardok/models/VardefInput.kt
@@ -9,6 +9,7 @@ data class VardefInput(
     var validFrom: String,
     val unitTypes: List<String?>,
     val externalReferenceUri: String,
+    val comment: String?,
     val containsSpecialCategoriesOfPersonalData: Boolean,
     val subjectFields: List<String?>,
     val classificationReference: String?,

d36eb643a5153e5e89e5ca6865ae2227effb3af0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-01-16 20:01:17 +0100

    uncorrect removed test

--- src/test/kotlin/no/ssb/metadata/vardef/controllers/validityperiods/ListTests.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/controllers/validityperiods/ListTests.kt
@@ -2,8 +2,10 @@ package no.ssb.metadata.vardef.controllers.validityperiods
 
 import io.micronaut.http.HttpStatus
 import io.restassured.specification.RequestSpecification
+import no.ssb.metadata.vardef.models.CompleteResponse
 import no.ssb.metadata.vardef.utils.BaseVardefTest
 import no.ssb.metadata.vardef.utils.INCOME_TAX_VP1_P1
+import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
@@ -23,6 +25,20 @@ class ListTests : BaseVardefTest() {
             .body("[0].patch_id", equalTo(7))
             .body("[1].valid_from", equalTo("2021-01-01"))
             .body("[1].patch_id", equalTo(6))
+    }
+
+    @Test
+    fun `list validity periods return type`(spec: RequestSpecification) {
+        val body =
+            spec
+                .`when`()
+                .get("/variable-definitions/${INCOME_TAX_VP1_P1.definitionId}/validity-periods")
+                .then()
+                .statusCode(HttpStatus.OK.code)
+                .extract()
+                .body()
+                .asString()
+        assertThat(jsonMapper.readValue(body, Array<CompleteResponse>::class.java)).isNotNull
     }
 
     @Test

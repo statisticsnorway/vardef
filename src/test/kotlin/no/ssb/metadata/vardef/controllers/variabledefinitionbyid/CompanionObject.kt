5a4fa7f3839b934c9819304aa67d04915ce76b53
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-01-17 00:46:55 +0100

    correct text

--- src/test/kotlin/no/ssb/metadata/vardef/controllers/variabledefinitionbyid/CompanionObject.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/controllers/variabledefinitionbyid/CompanionObject.kt
@@ -210,7 +210,7 @@ class CompanionObject {
                         .apply {
                             put("valid_until", "1970-11-12")
                         }.toString(),
-                    "Invalid date order.",
+                    "Invalid date order",
                 ),
                 argumentSet(
                     "valid from after valid until",
@@ -218,7 +218,7 @@ class CompanionObject {
                         .apply {
                             put("valid_from", "2041-02-22")
                         }.toString(),
-                    "Invalid date order.",
+                    "Invalid date order",
                 ),
             )
 

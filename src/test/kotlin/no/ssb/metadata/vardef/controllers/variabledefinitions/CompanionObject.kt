2ed34e25754352dbcc5fa2a866e28997456c5693
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-03 15:13:27 +0100

    add validator for LanguageStringType - always value in one language (#327)

--- src/test/kotlin/no/ssb/metadata/vardef/controllers/variabledefinitions/CompanionObject.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/controllers/variabledefinitions/CompanionObject.kt
@@ -2,6 +2,7 @@ package no.ssb.metadata.vardef.controllers.variabledefinitions
 
 import io.micronaut.http.HttpStatus
 import no.ssb.metadata.vardef.utils.jsonTestInput
+import org.json.JSONObject
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.Arguments.argumentSet
 import java.util.stream.Stream
@@ -34,6 +35,91 @@ class CompanionObject {
                             put("valid_until", "2024-06-05")
                         }.toString(),
                     HttpStatus.BAD_REQUEST,
+                ),
+            )
+
+        @JvmStatic
+        fun createDraftMandatoryFields(): Stream<Arguments> =
+            Stream.of(
+                argumentSet(
+                    "empty unit types",
+                    jsonTestInput()
+                        .apply {
+                            put("unit_types", listOf(""))
+                        }.toString(),
+                    HttpStatus.BAD_REQUEST,
+                ),
+                argumentSet(
+                    "empty subject fields",
+                    jsonTestInput()
+                        .apply {
+                            put("subject_fields", listOf(""))
+                        }.toString(),
+                    HttpStatus.BAD_REQUEST,
+                ),
+                argumentSet(
+                    "empty name",
+                    jsonTestInput()
+                        .apply {
+                            put(
+                                "name",
+                                JSONObject()
+                                    .apply {
+                                        put("nb", "")
+                                        put("nn", "")
+                                        put("en", "")
+                                    },
+                            )
+                        }.toString(),
+                    HttpStatus.BAD_REQUEST,
+                ),
+                argumentSet(
+                    "name one languages",
+                    jsonTestInput()
+                        .apply {
+                            put(
+                                "name",
+                                JSONObject()
+                                    .apply {
+                                        put("nb", "halleluja")
+                                        put("nn", "")
+                                        put("en", "")
+                                    },
+                            )
+                        }.toString(),
+                    HttpStatus.CREATED,
+                ),
+                argumentSet(
+                    "empty definition",
+                    jsonTestInput()
+                        .apply {
+                            put(
+                                "definition",
+                                JSONObject()
+                                    .apply {
+                                        put("nb", "")
+                                        put("nn", "")
+                                        put("en", "")
+                                    },
+                            )
+                        }.toString(),
+                    HttpStatus.BAD_REQUEST,
+                ),
+                argumentSet(
+                    "definition one language",
+                    jsonTestInput()
+                        .apply {
+                            put(
+                                "definition",
+                                JSONObject()
+                                    .apply {
+                                        put("nb", "")
+                                        put("nn", "")
+                                        put("en", "oh my holy bike")
+                                    },
+                            )
+                        }.toString(),
+                    HttpStatus.CREATED,
                 ),
             )
     }

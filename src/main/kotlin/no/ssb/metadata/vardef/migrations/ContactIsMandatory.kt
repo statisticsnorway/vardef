ae0b420f2c2b4eefa46f2607a131ebad0fee6b7a
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-05 12:18:01 +0100

    test file for updating Contact

---
+++ src/main/kotlin/no/ssb/metadata/vardef/migrations/ContactIsMandatory.kt
@@ -0,0 +1,49 @@
+package no.ssb.metadata.vardef.migrations
+
+import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Filters.*
+import com.mongodb.reactivestreams.client.MongoDatabase
+import io.mongock.api.annotations.ChangeUnit
+import io.mongock.api.annotations.Execution
+import io.mongock.api.annotations.RollbackExecution
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import com.mongodb.client.model.Updates.set
+import no.ssb.metadata.vardef.constants.GENERATED_CONTACT_KEYWORD
+import reactor.core.publisher.Mono
+
+@ChangeUnit(id = "set-default-value-to-contact", order = "002", author = "cbi")
+class ContactIsMandatory {
+    private val logger: Logger = LoggerFactory.getLogger(ContactIsMandatory::class.java)
+    @Execution
+    fun execution(mongoDatabase: MongoDatabase) {
+        val filter = and(
+            exists("contact"),
+            eq(null)
+        )
+        val updater =
+            mongoDatabase
+                .getCollection("SavedVariableDefinition")
+                .updateMany(filter, and(set("contact.email", "$GENERATED_CONTACT_KEYWORD@epost.com"),
+                    set("contact.title.nb", "$GENERATED_CONTACT_KEYWORD _tittel")))
+        Mono.from(updater).block().also { updateResult ->
+            if (updateResult != null) {
+                logger.info("Migration successful, updated ${updateResult.modifiedCount} documents")
+            } else {
+                logger.error("Migration result was null!")
+            }
+        }
+    }
+    @RollbackExecution
+    fun rollback(mongoDatabase: MongoDatabase) {
+        val filter = Filters.and(
+            exists("contact"),
+            eq(null)
+        )
+        val updater =
+            mongoDatabase
+                .getCollection("SavedVariableDefinition")
+                .updateMany(filter, set("contact.email", "$GENERATED_CONTACT_KEYWORD@epost.com"))
+        Mono.from(updater).block()
+    }
+}

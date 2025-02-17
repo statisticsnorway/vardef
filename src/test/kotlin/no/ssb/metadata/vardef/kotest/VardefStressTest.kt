d51ecc315f1dce48561228f609570c11c560429e
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-17 08:59:56 +0100

    refactor test - no need for several tests when spec always are executed sequentially

--- src/test/kotlin/no/ssb/metadata/vardef/kotest/VardefStressTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/kotest/VardefStressTest.kt
@@ -1,13 +1,10 @@
 package no.ssb.metadata.vardef.kotest
 
-import io.kotest.core.config.AbstractProjectConfig
-import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.exceptions.HttpStatusException
 import io.micronaut.security.authentication.Authentication
-import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
 import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 import kotlinx.coroutines.*
 import no.ssb.metadata.vardef.controllers.VariableDefinitionsController
@@ -22,16 +19,6 @@ import java.net.URI
 import java.time.LocalDate
 import kotlin.random.Random
 
-@Suppress("unused")
-object KotestConfig : AbstractProjectConfig() {
-    override val autoScanEnabled = false
-
-    override fun extensions() = listOf(MicronautKotest5Extension)
-
-    override val parallelism = 2
-    override val isolationMode = IsolationMode.InstancePerLeaf
-}
-
 @MicronautTest
 class VardefStressTest(
     private val variableDefinitionsController: VariableDefinitionsController,
@@ -39,79 +26,13 @@ class VardefStressTest(
 
         val logger = LoggerFactory.getLogger(VardefStressTest::class.java)
         val numOfDefinitions = 1000
-        val repeatCount = 4
+        val repeatCount = 2
 
         fun repeatTest(
             times: Int,
             block: suspend () -> Unit,
         ) = runBlocking {
             repeat(times) { block() }
-        }
-
-        should("create variable definitions") {
-            repeatTest(repeatCount) {
-                runBlocking {
-                    val result =
-                        (1..numOfDefinitions).map { i ->
-                            async {
-                                try {
-                                    val draft =
-                                        Draft(
-                                            name =
-                                                LanguageStringType(
-                                                    nb = "Buss",
-                                                    nn = "Buss",
-                                                    en = "Bus",
-                                                ),
-                                            shortName = "bus$i",
-                                            definition =
-                                                LanguageStringType(
-                                                    nb =
-                                                        "En buss er en bil for persontransport med over 8 sitteplasser " +
-                                                            "i tillegg til førersetet.",
-                                                    nn = null,
-                                                    en = "A bus is",
-                                                ),
-                                            classificationReference = "91",
-                                            unitTypes = listOf("02"),
-                                            subjectFields = listOf("al"),
-                                            containsSpecialCategoriesOfPersonalData = false,
-                                            measurementType = null,
-                                            validFrom = LocalDate.of(2025, 1, 1),
-                                            validUntil = null,
-                                            externalReferenceUri = URI("https://www.example.com").toURL(),
-                                            comment = null,
-                                            relatedVariableDefinitionUris =
-                                                listOf(
-                                                    URI("https://www.example.com").toURL(),
-                                                ),
-                                            contact =
-                                                Contact(
-                                                    LanguageStringType("Sjefen", "", ""),
-                                                    "sjef@ssb.no",
-                                                ),
-                                        )
-
-                                    variableDefinitionsController.createVariableDefinition(
-                                        draft = draft,
-                                        activeGroup = TEST_DEVELOPERS_GROUP,
-                                        authentication = Authentication.build(TEST_USER),
-                                    )
-                                } catch (e: HttpStatusException) {
-                                    e.status
-                                }
-                            }
-                        }.awaitAll()
-                    val successCount = result.count { it is CompleteResponse }
-                    val conflictCount = result.count { it == HttpStatus.CONFLICT }
-                    successCount shouldBe (numOfDefinitions - conflictCount)
-                }
-            }
-        }
-
-        should("return the correct number of variable definitions") {
-            val result = variableDefinitionsController.listVariableDefinitions()
-            result.size shouldBe numOfDefinitions
         }
 
         should("create variable definitions concurrently") {
@@ -124,7 +45,7 @@ class VardefStressTest(
                                 (1..numOfDefinitions).map { _ ->
                                     async {
                                         try {
-                                            val randomNum = Random.nextInt(100, 888)
+                                            val randomNum = Random.nextInt(1, 900)
                                             val draft =
                                                 Draft(
                                                     name =
@@ -172,9 +93,10 @@ class VardefStressTest(
                                         }
                                     }
                                 }.awaitAll()
-                            val successCount = result.count { it is CompleteResponse }
-                            val conflictCount = result.count { it == HttpStatus.CONFLICT }
-                            successCount shouldBe (numOfDefinitions - conflictCount)
+
+                                val successCount = result.count { it is CompleteResponse }
+                                val conflictCount = result.count { it == HttpStatus.CONFLICT }
+                                successCount shouldBe (numOfDefinitions - conflictCount)
                         }
                     }.awaitAll()
                 }

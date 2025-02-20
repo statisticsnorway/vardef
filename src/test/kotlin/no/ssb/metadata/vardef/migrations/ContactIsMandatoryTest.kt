f9a9007d667e095680ffd93fe848dfe4e0fb93b9
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-02-20 14:19:57 +0100

    Clear the db before the test too

--- src/test/kotlin/no/ssb/metadata/vardef/migrations/ContactIsMandatoryTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/migrations/ContactIsMandatoryTest.kt
@@ -24,6 +24,7 @@ class ContactIsMandatoryTest {
         mongoDatabase = mongoClient.getDatabase(MONGO_DB_NAME_VARDEF)
     }
 
+    @BeforeEach
     @AfterEach
     fun resetDatabase() {
         Mono.from(mongoDatabase.drop()).block()

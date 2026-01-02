9381a1cfdc86b3d115e42884b31bfe9b0c723748
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-02 12:44:57 +0100

    remove mock

--- src/test/kotlin/no/ssb/metadata/vardef/services/MetricsServiceTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/services/MetricsServiceTest.kt
@@ -1,12 +1,6 @@
 package no.ssb.metadata.vardef.services
 
-import io.micronaut.context.annotation.Factory
-import io.micronaut.context.annotation.Replaces
 import jakarta.inject.Inject
-import jakarta.inject.Singleton
-import no.ssb.metadata.vardef.integrations.dapla.models.Group
-import no.ssb.metadata.vardef.integrations.dapla.models.Team
-import no.ssb.metadata.vardef.integrations.dapla.services.DaplaTeamService
 import no.ssb.metadata.vardef.integrations.vardok.models.VardokVardefIdPair
 import no.ssb.metadata.vardef.services.metrics.MigrationMetricsCalculator
 import no.ssb.metadata.vardef.utils.*
@@ -20,8 +14,10 @@ class MetricsServiceTest : BaseVardefTest() {
 
     @BeforeEach
     fun setUpMetrics() {
+        // SAVED_DRAFT_DEADWEIGHT_EXAMPLE owner Team is "skip-stat"
         vardokIdMappingRepository.save(VardokVardefIdPair("006", SAVED_DRAFT_DEADWEIGHT_EXAMPLE.definitionId))
 
+        // DRAFT_BUS_EXAMPLE owner Team is  "play-enhjoern-a"
         // DRAFT_BUS_EXAMPLE is edited after it was migrated
         val editedVariable =
             DRAFT_BUS_EXAMPLE.copy(
@@ -34,8 +30,8 @@ class MetricsServiceTest : BaseVardefTest() {
     fun `count migrated variables per section`() {
         val expected =
             mapOf(
-                "724" to 1,
-                "Unknown" to 1,
+                "play-enhjoern-a" to 1,
+                "skip-stat" to 1,
             )
 
         assertThat(vardokIdMappingRepository.count())
@@ -52,8 +48,8 @@ class MetricsServiceTest : BaseVardefTest() {
     fun `count edited migrated variables per section`() {
         val expected =
             mapOf(
-                "724" to 1,
-                "Unknown" to 0,
+                "play-enhjoern-a" to 1,
+                "skip-stat" to 0,
             )
         assertThat(metricsCalculator.countEditedMigratedBySection())
             .withFailMessage(
@@ -64,25 +60,4 @@ class MetricsServiceTest : BaseVardefTest() {
             .withFailMessage("Total edited migrated should be 1")
             .isEqualTo(1)
     }
-}
-
-@Factory
-@Replaces(DaplaTeamService::class)
-class TestDaplaTeamFactory {
-    @Singleton
-    fun daplaTeamService(): DaplaTeamService =
-        object : DaplaTeamService {
-            override fun getTeam(teamName: String) =
-                when (teamName) {
-                    "play-enhjoern-a" -> Team("724", sectionCode = "724", sectionName = "Seksjon for dataplattform")
-                    "skip-stat" -> Team("0", sectionCode = "Unknown", sectionName = "Unknown")
-                    else -> null
-                }
-
-            override fun isValidTeam(teamName: String) = getTeam(teamName) != null
-
-            override fun getGroup(groupName: String) = Group(groupName)
-
-            override fun isValidGroup(groupName: String) = true
-        }
 }

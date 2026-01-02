299dd27ca9f510a88872379876ebf6ff99d838df
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-02 09:36:17 +0100

    refactor test

--- src/test/kotlin/no/ssb/metadata/vardef/services/MetricsServiceTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/services/MetricsServiceTest.kt
@@ -1,26 +1,58 @@
 package no.ssb.metadata.vardef.services
 
 import jakarta.inject.Inject
+import no.ssb.metadata.vardef.integrations.vardok.models.VardokVardefIdPair
 import no.ssb.metadata.vardef.services.metrics.MigrationMetricsCalculator
 import no.ssb.metadata.vardef.utils.*
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class MetricsServiceTest : BaseVardefTest() {
+class MetricsServiceTest: BaseVardefTest() {
     @Inject
     private lateinit var metricsCalculator: MigrationMetricsCalculator
 
-    @Test
-    fun `map migrated variables`() {
-        val countMigrated = metricsCalculator.countMigratedVariablesBySection()
-        assertThat(countMigrated.size).isEqualTo(2)
-        assertThat(countMigrated.keys).containsExactlyInAnyOrder("724", "Unknown")
-        assertThat(countMigrated["724"]).isEqualTo(1)
+    @BeforeEach
+    fun setUpMetrics() {
+        // SAVED_DRAFT_DEADWEIGHT_EXAMPLE has not valid team -> will be counted as "Unknown"
+        vardokIdMappingRepository.save(VardokVardefIdPair("006", SAVED_DRAFT_DEADWEIGHT_EXAMPLE.definitionId))
+
+        // DRAFT_BUS_EXAMPLE is assigned to team 724
+        // and is edited after it was migrated
+        val editedVariable = DRAFT_BUS_EXAMPLE.copy(
+            lastUpdatedAt = DRAFT_BUS_EXAMPLE.lastUpdatedAt.plusSeconds(3600)
+        )
+        variableDefinitionRepository.update(editedVariable)
     }
 
     @Test
-    fun `count edited migrated variables`() {
-        assertThat(DRAFT_BUS_EXAMPLE.createdAt).isNotEqualTo(DRAFT_BUS_EXAMPLE.lastUpdatedAt)
-        assertThat(metricsCalculator.countEditedMigratedBySection()).isEqualTo("dvkt")
+    fun `count migrated variables per section`() {
+        val expected = mapOf(
+            "724" to 1,
+            "Unknown" to 1
+        )
+
+        assertThat(vardokIdMappingRepository.count())
+            .withFailMessage("Repository should contain 2 variable mappings")
+            .isEqualTo(2)
+
+        assertThat(metricsCalculator.countMigratedVariablesBySection())
+            .withFailMessage("Count of migrated variables per section should match $expected")
+            .isEqualTo(expected)
+    }
+
+    @Test
+    fun `count edited migrated variables per section`() {
+        val expected = mapOf(
+            "724" to 1,
+            "Unknown" to 0
+        )
+        assertThat(metricsCalculator.countEditedMigratedBySection())
+            .withFailMessage("Count of edited migrated variables per section should match $expected")
+            .isEqualTo(expected)
+
+        assertThat(metricsCalculator.countTotalEditedMigrated())
+            .withFailMessage("Total edited migrated should be 1")
+            .isEqualTo(1)
     }
 }

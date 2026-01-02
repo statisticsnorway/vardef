d0d45d47cec1a5ee8f9b862dada973ca85c350a8
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-02 11:35:08 +0100

    try to mock dapla team api

--- src/test/kotlin/no/ssb/metadata/vardef/services/MetricsServiceTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/services/MetricsServiceTest.kt
@@ -1,14 +1,43 @@
 package no.ssb.metadata.vardef.services
 
+import io.micronaut.context.annotation.Replaces
 import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import no.ssb.metadata.vardef.integrations.dapla.models.Group
+import no.ssb.metadata.vardef.integrations.dapla.models.Team
+import no.ssb.metadata.vardef.integrations.dapla.services.DaplaTeamApiService
+import no.ssb.metadata.vardef.integrations.dapla.services.DaplaTeamService
 import no.ssb.metadata.vardef.integrations.vardok.models.VardokVardefIdPair
 import no.ssb.metadata.vardef.services.metrics.MigrationMetricsCalculator
 import no.ssb.metadata.vardef.utils.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 
 class MetricsServiceTest : BaseVardefTest() {
+
+    private val logger = LoggerFactory.getLogger(MetricsServiceTest::class.java)
+    @Singleton
+    @Replaces(DaplaTeamApiService::class)
+    fun mockDaplaTeamService(): DaplaTeamService =
+        object : DaplaTeamService {
+            override fun getTeam(teamName: String): Team? {
+                logger.info("Mock get Team for $teamName")
+                return when (teamName) {
+                    "play-enhjoern-a" -> Team("724", sectionCode = "724", sectionName = "Seksjon for dataplattform")
+                    "skip-stat" -> Team("0", sectionCode = "Unknown", sectionName = "Unknown")
+                    else -> null
+                }
+            }
+
+            override fun isValidTeam(teamName: String) = true
+
+            override fun getGroup(groupName: String) = Group(groupName)
+
+            override fun isValidGroup(groupName: String) = true
+        }
+
     @Inject
     private lateinit var metricsCalculator: MigrationMetricsCalculator
 
@@ -39,7 +68,7 @@ class MetricsServiceTest : BaseVardefTest() {
             .isEqualTo(2)
 
         assertThat(metricsCalculator.countMigratedVariablesBySection())
-            .withFailMessage("Count of migrated variables per section should match $expected")
+            .withFailMessage("Count of migrated variables per section ${metricsCalculator.countMigratedVariablesBySection()} should match expected: $expected")
             .isEqualTo(expected)
     }
 
@@ -51,7 +80,7 @@ class MetricsServiceTest : BaseVardefTest() {
                 "Unknown" to 0,
             )
         assertThat(metricsCalculator.countEditedMigratedBySection())
-            .withFailMessage("Count of edited migrated variables per section should match $expected")
+            .withFailMessage("Count of edited migrated variables per section ${metricsCalculator.countEditedMigratedBySection()} should match expected: $expected")
             .isEqualTo(expected)
 
         assertThat(metricsCalculator.countTotalEditedMigrated())

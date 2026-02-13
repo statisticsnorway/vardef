d42cfc7e6c55cf6a8b1c92eb5998a07f8ccdb003
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-02-13 14:41:24 +0100

    Workaround for Testcontainers incompatibility

--- src/test/kotlin/no/ssb/metadata/vardef/utils/BaseVardefTest.kt
+++ src/test/kotlin/no/ssb/metadata/vardef/utils/BaseVardefTest.kt
@@ -13,6 +13,7 @@ import no.ssb.metadata.vardef.repositories.VariableDefinitionRepository
 import no.ssb.metadata.vardef.services.PatchesService
 import no.ssb.metadata.vardef.services.ValidityPeriodsService
 import no.ssb.metadata.vardef.services.VariableDefinitionService
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.TestInstance
 

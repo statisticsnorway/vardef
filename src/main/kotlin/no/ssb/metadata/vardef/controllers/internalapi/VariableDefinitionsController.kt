7b72a2fb764660ac7136ab2bbbd40d32a81e3d7e
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-09-17 11:56:31 +0200

    Document GET operations as unauthenticated

--- src/main/kotlin/no/ssb/metadata/vardef/controllers/internalapi/VariableDefinitionsController.kt
+++ src/main/kotlin/no/ssb/metadata/vardef/controllers/internalapi/VariableDefinitionsController.kt
@@ -14,6 +14,7 @@ import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule.IS_ANONYMOUS
 import io.micronaut.validation.Validated
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
@@ -68,6 +69,7 @@ class VariableDefinitionsController(
     )
     @Get
     @Secured(IS_ANONYMOUS, Roles.VARIABLE_CONSUMER)
+    @Operation(security = [])
     fun listVariableDefinitions(
         @Parameter(
             description = ACCEPT_LANGUAGE_HEADER_PARAMETER_DESCRIPTION,
